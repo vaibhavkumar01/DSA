33287720587a11e9f05d87dc9e213ce284d70fc7
vaibhavkumar01 <vaibhavkumar1393@gmail.com>
2025-09-21 22:02:15 +0530

    Added right triangle star patter

---
+++ Basic C++/right_triangle_star_pattern.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,row,col;
+
+    cout<< "Input a Number: ";
+    cin >> n;
+
+    // outer loop
+    for(row=1;row<=n;row++){
+        for(col=1;col<=row;col++){
+            cout << "* ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
