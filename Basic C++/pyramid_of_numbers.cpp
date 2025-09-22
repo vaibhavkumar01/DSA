903705c139262b34dfde1a5645ca3ba3b0856926
vaibhavkumar01 <vaibhavkumar1393@gmail.com>
2025-09-22 11:56:24 +0530

    Added Pyramid of numbers

--- Basic C++/pyramid_of_numbers.cpp
+++ Basic C++/pyramid_of_numbers.cpp
@@ -2,6 +2,17 @@
 using namespace std;
 int main()
 {
+//     Enter a number: 9
+//                 1 
+//               1 2 1
+//             1 2 3 2 1
+//           1 2 3 4 3 2 1
+//         1 2 3 4 5 4 3 2 1 
+//       1 2 3 4 5 6 5 4 3 2 1
+//     1 2 3 4 5 6 7 6 5 4 3 2 1
+//   1 2 3 4 5 6 7 8 7 6 5 4 3 2 1
+// 1 2 3 4 5 6 7 8 9 8 7 6 5 4 3 2 1
+
     int n,row,col;
 
     cout << "Enter a number: ";
