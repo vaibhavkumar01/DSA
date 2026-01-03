366eed85861f41cb6a3ba1bc00ba0a5503af2105
vaibhavkumar01 <vaibhavkumar1393@gmail.com>
2026-01-03 18:50:48 +0530

    Added Selection Sort concept example

---
+++ DSA Concepts/Selection_Sort.cpp
@@ -0,0 +1,60 @@
+#include<iostream>
+using namespace std;
+
+// int main()
+// {
+//     int arr[1000];
+//     int n;
+//     cout << "Enter the size of Array: ";
+//     cin >> n;
+//     cout << "Enter the element in Array: ";
+//     for(int i=0; i<n; i++)
+//     cin >> arr[i];
+
+//     for(int i=0; i<n-1; i++)
+//     {
+//         int index = i;
+//         for(int j=i+1; j<n; j++)
+//         {
+//             if(arr[j] < arr[index])
+//             index = j;
+//         }
+
+//         swap(arr[i],arr[index]);
+//     }
+
+//     for(int i=0; i<n; i++){
+//         cout << arr[i] << " ";
+//     }
+    
+//     return 0;
+// }
+
+int main()
+{
+    int arr[1000];
+    int n;
+    cout << "Enter the size of Array: ";
+    cin >> n;
+    cout << "Enter the element in Array: ";
+    for(int i=0; i<n; i++)
+    cin >> arr[i];
+
+    for(int i=0; i<n-1; i++)
+    {
+        int index = i;
+        for(int j=i+1; j<n; j++)
+        {
+            if(arr[j] > arr[index])
+            index = j;
+        }
+
+        swap(arr[i],arr[index]);
+    }
+
+    for(int i=0; i<n; i++){
+        cout << arr[i] << " ";
+    }
+    
+    return 0;
+}
